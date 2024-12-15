96cf04c82632ec0c9ba7eff11c7080bf2f7b1914
Thomas Huss <45053302+thomashuss@users.noreply.github.com>
2024-12-15 01:25:28 -0500

    Options input validation

--- extension/src/options/options.ts
+++ extension/src/options/options.ts
@@ -16,32 +16,91 @@
 
 import browser from "webextension-polyfill";
 
+function validate() {
+    const errors: string[] = [];
+
+    function validateCondition(condition: boolean, errorMessage: string) {
+        if (!condition) {
+            errors.push(errorMessage);
+        }
+    }
+
+    const problemFileSuffix = (document.getElementById("problem_file_suffix") as HTMLInputElement | null)?.value;
+    const selectedConverter = (document.getElementById("problem_converter") as HTMLSelectElement | null)?.value;
+    validateCondition(
+        selectedConverter !== "open_html_to_pdf" || problemFileSuffix === ".pdf",
+        "Problem file extension must be `.pdf' when using Open HTML to PDF."
+    );
+    validateCondition(
+        selectedConverter !== "raw_html" || problemFileSuffix === ".html",
+        "Problem file extension must be `.html' when using raw HTML."
+    );
+    validateCondition(
+        selectedConverter !== "pandoc"
+        || (document.getElementById("pandoc_path") as HTMLInputElement | null)?.value.trim() !== "",
+        "Pandoc path must be specified when using Pandoc."
+    );
+    validateCondition(
+        selectedConverter !== "libreoffice"
+        || (document.getElementById("libreoffice_path") as HTMLInputElement | null)?.value.trim() !== "",
+        "LibreOffice path must be specified when using LibreOffice."
+    );
+    validateCondition(
+        (document.getElementById("write_test_case_to_temp_file") as HTMLInputElement | null)?.checked
+        || (document.getElementById("test_case_file_path") as HTMLInputElement | null)?.value.trim() !== "",
+        "Test case file path must be specified if not writing test cases to a temporary file."
+    );
+    validateCondition(
+        (document.getElementById("write_code_to_temp_file") as HTMLInputElement | null)?.checked
+        || (document.getElementById("code_file_path") as HTMLInputElement | null)?.value.trim() !== "",
+        "Code file path must be specified if not writing code to a temporary file."
+    );
+    validateCondition(
+        (document.getElementById("write_problem_to_temp_file") as HTMLInputElement | null)?.checked
+        || (document.getElementById("problem_file_path") as HTMLInputElement | null)?.value.trim() !== "",
+        "Problem file path must be specified if not writing problems to a temporary file."
+    );
+
+    if (errors.length > 0) {
+        alert("Correct the following errors:\n" + errors.join("\n"));
+        return false;
+    }
+    return true;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const elems = document.getElementsByClassName("pref");
-    browser.storage.local.get(null).then((prefs) => {
-        for (const e of elems) {
-            const p = prefs[e.id] as string;
-            if (p && (e instanceof HTMLInputElement || e instanceof HTMLSelectElement)) {
-                if (e.type === "checkbox") {
-                    e.checked = p === "true";
-                } else {
-                    e.value = p;
+    const saveBtn = document.getElementById("save") as HTMLButtonElement | null;
+    if (elems.length > 0 && saveBtn != null) {
+        browser.storage.local.get(null).then((prefs) => {
+            for (const e of elems) {
+                const p = prefs[e.id] as string;
+                if (p && (e instanceof HTMLInputElement || e instanceof HTMLSelectElement)) {
+                    if (e.type === "checkbox") {
+                        e.checked = p === "true";
+                    } else {
+                        e.value = p;
+                    }
                 }
+                e.addEventListener("change", () => saveBtn.disabled = false);
             }
-        }
-    });
+        });
 
-    document.getElementById("save")?.addEventListener("click", () => {
-        const p: Record<string, string> = {};
-        for (const e of elems) {
-            if (e instanceof HTMLInputElement || e instanceof HTMLSelectElement) {
-                if (e.type === "checkbox") {
-                    p[e.id] = e.checked ? "true" : "false";
-                } else {
-                    p[e.id] = e.value;
+        saveBtn.addEventListener("click", () => {
+            saveBtn.disabled = true;
+            if (validate()) {
+                const p: Record<string, string> = {};
+                for (const e of elems) {
+                    if (e instanceof HTMLInputElement || e instanceof HTMLSelectElement) {
+                        if (e.type === "checkbox") {
+                            p[e.id] = e.checked ? "true" : "false";
+                        } else {
+                            p[e.id] = e.value;
+                        }
+                    }
                 }
+                browser.storage.local.set(p);
             }
-        }
-        browser.storage.local.set(p);
-    });
+        });
+    }
 });
